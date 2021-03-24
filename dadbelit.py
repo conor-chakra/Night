47e9d99ab2914acfc11985bb154c7287678e0065
conor-chakra <conor.chakraborty@github.com>
2021-03-24 20:58:15 +0000

    ✨ i learned how to do a loop

--- dadbelit.py
+++ dadbelit.py
@@ -1,4 +1,5 @@
-if 3>5:
-    print('dad is a big loser')
-elif 7<8:
-    print ('concon aint a big loser')    
+#This isknow as a list 
+desk = [1,2,3,4,5]
+
+for item in desk:
+    print(item +6027)
