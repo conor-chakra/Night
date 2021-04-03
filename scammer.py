c86e21fdda843d826561abc31b2893d359c336d1
conor-chakra <conor.chakraborty@github.com>
2021-04-03 19:12:12 +0100

    📝 Finished

--- scammer.py
+++ scammer.py
@@ -1,5 +1,4 @@
 
 username = input('Hello. If you would like to continue please enter your username? ')
-print(username)
-genius = input(' Thank you for entering your username. Now can you please write noot if you would like to have £1000 transfered to your bank account. ')
+genius = input('Thank you for entering your username. Now can you please write yes if you would like to have £1000 transfered to your bank account. ')
 print(' \n Hello ' + str(username) + '. We will now deduct £1000 from your account. Thank you for your money. \n ')
