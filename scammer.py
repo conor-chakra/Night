2264510b2e4268f4c31d35a7ab20d7ffea4c00da
conor-chakra <conor.chakraborty@github.com>
2021-04-03 19:02:16 +0100

    I will get you to mail me 'mooney' as charlie would say

--- scammer.py
+++ scammer.py
@@ -1,4 +1,5 @@
 
 username = input('Hello. If you would like to continue please enter your username? ')
 print(username)
-print(' \n Hello ' + str(username) + '. Thank you for entering your username. We will now deduct £1000 from your account \n ')
+genius = input(' Thank you for entering your username. Now can you please write noot if you would like to have £1000 transfered to your bank account. ')
+print(' \n Hello ' + str(username) + '. We will now deduct £1000 from your account. Thank you for your money. \n ')
