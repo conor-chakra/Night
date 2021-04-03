b0a657c17d964ccb69187b9f4dee63fe7b14d434
conor-chakra <conor.chakraborty@github.com>
2021-04-03 18:10:18 +0100

    ⚡️ well done scammer boy

--- scammer.py
+++ scammer.py
@@ -1,5 +1,4 @@
 
 username = input('Hello. If you would like to continue please enter your username? ')
-x.insert(3,username)
-print(x)
-print(' \n Hello ' + str(username) + '. Thank you for entering your username. We will now deduct £1000 from your account \n    ')
+print(username)
+print(' \n Hello ' + str(username) + '. Thank you for entering your username. We will now deduct £1000 from your account \n ')
