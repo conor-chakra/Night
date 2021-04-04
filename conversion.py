4adab258f8b3d0cf26728804c22477579c648064
conor-chakra <conor.chakraborty@github.com>
2021-04-04 13:33:41 +0100

    ✅ refined

--- conversion.py
+++ conversion.py
@@ -1,3 +1,9 @@
-weight = input('how much do you weight in kilograms: ')
+weight = input('how much do you weigh in kilograms: ')
 pounds = 2.205 * int(weight)
-print(pounds)
+weight_in_pound = f'{pounds} is your weight in pounds'
+print(weight_in_pound)
+
+birth_year = input('what is your birth year? ')
+age = 2021 - int(birth_year)
+year = f'{age} is their age'
+print(year)
