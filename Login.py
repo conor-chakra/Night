7fd73b9337a91fd5770ff90a636ea8a5dbe35d08
conor-chakra <conor.chakraborty@github.com>
2021-04-04 17:16:14 +0100

    boom

--- Login.py
+++ Login.py
@@ -1,5 +1,9 @@
 name = input ('Please enter your name ')
 if len(name)< 3:
     print('Your name must be a mininum of 3 letters')
+    name = input ('Please enter your name ')
 if len (name)>20:
     print ('Your name must be under 20 letters')
+    name = input ('Please enter your name ')
+else:
+        print('Welcome to the game '+ name)
